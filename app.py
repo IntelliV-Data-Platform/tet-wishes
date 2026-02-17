297ce63e75bc81d4e93748a31f14de18bc4f2cb4
IntelliV-Data-Platform <an.tranbinh@hotmail.com>
2026-02-17 09:30:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ st.markdown("""
     <h1 style='
         text-align: center; 
         color: #D4AF37; 
-        font-size: 24px;
+        font-size: 22px;
         text-overflow: ellipsis;
         margin-top: 0px;
         padding-top: 10px;
